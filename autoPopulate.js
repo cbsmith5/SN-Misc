1e38e5d408aff65a2e7b593d0ff3eaa9db8a2526
Cheyenne Smith <30705498+cbsmith5@users.noreply.github.com>
2019-10-17 15:10:30 -0400

    Create autoPopulate.js

---
+++ autoPopulate.js
@@ -0,0 +1,14 @@
+//This is used to auto-populate a reference variable based on another reference variable.
+function onChange(control, oldValue, newValue, isLoading) {
+	// If tla is empty, clear all related values
+	if (newValue == '') {
+		g_form.setValue('tla_name', '');
+	} else if (newValue != '') {
+		// Otherwise, get the customer account from the record:
+		var requestName = g_form.getReference('tla', getNameFrom);
+	}
+	//set customer account
+	function getNameFrom(requestFrom) {
+		g_form.setValue('tla_name', requestFrom.u_customer_account);
+	}
+}
